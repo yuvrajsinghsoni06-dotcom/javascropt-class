3bc19e3cc80624ac1fbc48b7d76c7fd85837c246
Yuvrajsinghsoni <enter>
2026-01-31 13:51:06 +0530

    Add initial asynchronous JavaScript example

---
+++ javascript.js
@@ -0,0 +1,31 @@
+// // // // Asynchonous js 
+// // // console.log("first line")
+// // // setTimeout(()=>{
+// // //     console.log("after 2 seconds")      // Asynchoronnous behaviour- covers the tasks in background and executes after the main code is executed
+// // // },2000)
+// // // console.log("second line")
+// // // //  handling asynchronous 
+// // // // timers
+// // setTimeout(()=>{
+// //     alert("Alert after 3 seconds")
+// // },3*1000)
+
+// //  const timerId = setInterval(()=>{
+// //     console.log("Hello students!!")
+// // },2000)
+
+ 
+// // setTimeout(()=>{
+// //     clearInterval(timerId)
+// // },10*1000 )// to stop the interval
+
+// let count = 1
+// const timerid=setInterval(()=>{
+//     console.log(count)
+//     count++;
+//     if (count > 10){
+//         clearInterval(timerid)
+//     }
+// },1000);
+
+
